d1b2d012951797e2d45e6ef63624baf91e20d5ec
carlosalberto05 <carlos_alberto_lira@hotmail.com>
2020-08-18 21:21:52 -0500

    Se agregó calculo de iva

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css'
 
 function App() {
   const [cantidad, setCantidad] = useState(0)
-  const [resultado, setResultado] = useState(null)
+  const [subtotal, setSubtotal] = useState(null)
+  const [totalIva, setTotalIva] = useState(null)
 
   const handleChange = (e) => {
     e.preventDefault()
     setCantidad(e.target.value)
   }
 
-  const subtotal = (e) => {
+  const calculo = (e) => {
     e.preventDefault()
     if (cantidad > 0) {
       let sub = cantidad / 1.16
-      setResultado(sub.toFixed(2))
+      let iva = sub * 0.16
+      setSubtotal(sub.toFixed(2))
+      setTotalIva(iva.toFixed(2))
     } else {
       return 'No hay datos suficientes'
     }
@@ -30,10 +33,11 @@ function App() {
           value={cantidad}
           onChange={handleChange}
         />
-        <input type="submit" onClick={subtotal} />
+        <input type="submit" onClick={calculo} />
       </form>
       <div>
-        <h4>El subtotal es: {resultado}</h4>
+        <h4>El subtotal es: {subtotal}</h4>
+        <h4>El iva es: {totalIva}</h4>
       </div>
     </div>
   )
